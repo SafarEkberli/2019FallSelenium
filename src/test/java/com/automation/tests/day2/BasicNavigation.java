683ce0c71c232c1a7c967d5abf646d6c42b26aec
SafarEkberli <safarmaharramov@gmail.com>
2020-03-03 15:48:46 -0500

    created basic navigation class First Automation test

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -6,10 +6,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
+        Thread.sleep(3000);
+        String title = driver.getTitle();
+        String expectedTitle = "Google";
+
+        System.out.println("Title is "+title);
+        if(expectedTitle.equals(title)){
+            System.out.println("Test Passed!");
+        }else {
+            System.out.println("Test Failed!");
+        }
+        driver.close();
+
 
     }
 }
